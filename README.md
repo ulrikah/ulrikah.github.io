dabb2afa745a5f8962f200c7173e08f0a2920cc4
ulrikah <25346380+ulrikah@users.noreply.github.com>
2020-11-22 19:44:53 +0000

    Deploying to gh-pages from  @ fedf1cb75bb55d832cfd3acdea59d1d993a68800 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-# ulrikah.github.io
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-console.log("Hello types")
-
-
-const header = document.createElement("h1");
-header.textContent = "Webpage"
-
-document.body.appendChild(header)
-
